c24788110a2a52dde78a7069a769ae71caaf1e6c
Natan200-2 <natanboas21@outlook.com>
2024-01-11 15:59:19 -0300

    Forms conclused

--- src/components/Forms/index.js
+++ src/components/Forms/index.js
@@ -1,13 +1,17 @@
 import styles from './Form.module.css';
 import InputField from '../InputField';
+import { Link } from 'react-router-dom';
 
 function Form() {
     return(
         <section className={styles.container}>
+            <Link to='/'>
+            <button className={styles.backButton}>Voltar</button>
+            </Link>
             <form>
                 <InputField label='Titulo' placeholder='Digite um título...'/>
                 <InputField label='Decrição' placeholder='Digite a descrição...'/>
-                <button>Salvar</button>
+                <button>Salvar Tarefa</button>
             </form>
         </section>
     )
