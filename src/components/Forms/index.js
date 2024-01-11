99979069d8893b24b724eac9a676d48492b4fc78
Natan200-2 <natanboas21@outlook.com>
2024-01-11 16:24:05 -0300

    saved tasks info on useState

--- src/components/Forms/index.js
+++ src/components/Forms/index.js
@@ -1,16 +1,33 @@
 import styles from './Form.module.css';
 import InputField from '../InputField';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Form() {
+    const [title, setTitle] = useState('');
+    const [desc, setDesc] = useState('');
+
+    const onSave = (e) =>{
+        e.preventDefault();
+        
+    }
+
     return(
         <section className={styles.container}>
             <Link to='/'>
             <button className={styles.backButton}>Voltar</button>
             </Link>
-            <form>
-                <InputField label='Titulo' placeholder='Digite um título...'/>
-                <InputField label='Decrição' placeholder='Digite a descrição...'/>
+            <form onSubmit={onSave}>
+                <InputField 
+                value={title}
+                label='Titulo' 
+                placeholder='Digite um título...' 
+                saveInfo={value => setTitle(value)}/>
+                <InputField 
+                value={desc}
+                label='Decrição' 
+                placeholder='Digite a descrição...' 
+                saveInfo={value => setDesc(value)}/>
                 <button>Salvar Tarefa</button>
             </form>
         </section>
--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -1,10 +1,15 @@
 import styles from './InputField.module.css';
 
 function InputField(props){
+
+    const onDigit= (e) =>{
+        props.saveInfo(e.target.value)    
+    }
+
     return(
         <div className={styles.container}>
             <label>{props.label}</label>
-            <input placeholder={props.placeholder}/>
+            <input onChange={onDigit} placeholder={props.placeholder}/>
         </div>
     )
 }
