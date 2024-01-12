bccf879108e483bf261a122758b5ea5ed5d6fa31
Natan200-2 <natanboas21@outlook.com>
2024-01-12 16:43:58 -0300

    animated task conclused action

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,16 +1,20 @@
-import { useTasksContext } from '../../context/Tasks';
+import { useState } from 'react';
 import styles from './Task.module.css';
 import penIcon from './ferramenta-lapis.png';
 import checkIcon from './marca-de-verificacao.png';
 
 function Task(props) {
+    const [conclused, setConclused] = useState(false);
 
     const handleRemoveTask = (task) => {
-        props.removedTask(task);
+        setConclused(true);
+        setTimeout(() => {
+            props.removedTask(task);
+          }, 1000);
     }
 
     return(
-        <div className={styles.container}>
+        <div className={`${styles.container} ${conclused === true ? styles.conclused : ''}`}>
             <div className={styles.containerText}>
                 <h5 className={styles.title}>{props.title}</h5>
                 <p className={styles.text}>{props.text}</p>
