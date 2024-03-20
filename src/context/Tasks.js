ba1b3f2567c953d0f1307683c7f52586c3086ee0
Natan200-2 <natanboas21@outlook.com>
2024-03-20 15:00:07 -0300

    🎨 🔥 limpando código e ajustando formatação

--- src/context/Tasks.js
+++ src/context/Tasks.js
@@ -1,40 +1,40 @@
 import { createContext, useContext, useState } from "react"
-import {v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
 
 export const TasksContext = createContext();
 TasksContext.displayName = "Tasks";
 
-export default function TasksProvider({children}){
+export default function TasksProvider({ children }) {
     const [tasks, setTasks] = useState([]);
 
-    return(
-        <TasksContext.Provider value={{tasks, setTasks}}>
+    return (
+        <TasksContext.Provider value={{ tasks, setTasks }}>
             {children}
         </TasksContext.Provider>
     )
 }
 
-export function useTasksContext(){
-    const {tasks, setTasks} = useContext(TasksContext);
+export function useTasksContext() {
+    const { tasks, setTasks } = useContext(TasksContext);
 
-    function addTask(newTask){
+    function addTask(newTask) {
         const taskWithId = { ...newTask, id: uuidv4() };
-    
+
         let newList = [...tasks, taskWithId];
         setTasks(newList);
     }
 
     function removeTask(taskId) {
         setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
-      }
+    }
 
-      function editTask(taskToEdit) {
+    function editTask(taskToEdit) {
         let newList = tasks.map((task) => (task.id === taskToEdit.id ? { ...taskToEdit } : task));
         setTasks(newList);
-      }
+    }
 
-    return{
+    return {
         tasks,
         addTask,
         removeTask,
